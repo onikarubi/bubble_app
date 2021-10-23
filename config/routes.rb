db87fecd047cfb47843986dbdb4d368024c75a0b
onikarubi <printuserselect@gmail.com>
2021-10-23 13:11:00 +0900

    articles controller
    articlesコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: %i[show update edit]
+
+  devise_scope :user do
+    root to: 'articles#index'
+  end
+
+  resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
