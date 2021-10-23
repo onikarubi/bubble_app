5c4be3c4094a0153aa4ae3c2f105815340f2906b
onikarubi <printuserselect@gmail.com>
2021-10-23 11:53:22 +0900

    devise controller config
    devise controller の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email image])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[name])
+  end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 end
